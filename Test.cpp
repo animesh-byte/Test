bf05825bc74e0d72602fea1ccb5ef51a9a42715c
animesh-byte <70771777+animesh-byte@users.noreply.github.com>
2022-12-24 14:06:24 +0530

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+void display(vector<vector<int>>&arr){
+    for(int i=0;i<arr.size();i++){
+        for(int j=0;j<arr[0].size();j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
 vector<vector<int>> create_matrix(int n,int m){
     vector<vector<int>> v(n,vector<int>(m,0));
     for(int i=0;i<n;i++){
@@ -10,79 +19,42 @@ vector<vector<int>> create_matrix(int n,int m){
     }
     return v;
 }
-
-void Merge(vector<vector<int>> &vec,int start,int mid,int end, int col){
-    auto const subArrayOne = mid - start + 1;
-    auto const subArrayTwo = end - mid;
- 
-    vector<int> arr1(subArrayOne), arr2(subArrayTwo);
-    
-    for (auto i = 0; i < subArrayOne; i++)
-        arr1[i] = vec[start + i][col];
-    for (auto j = 0; j < subArrayTwo; j++)
-        arr2[j] = vec[mid + 1 + j][col];
- 
-    auto ptr1 = 0, ptr2 = 0; 
-    int ptr3 = start; 
-
-    while (ptr1 < subArrayOne && ptr2 < subArrayTwo) {
-        if (arr1[ptr1] <= arr2[ptr2]) { 
-                vec[ptr3][col] = arr1[ptr1];
-            ptr1 += 1;
-        }
-        else {
-            vec[ptr3][col] = arr2[ptr2];
-            ptr2 += 1;
-        }
-        ptr3 += 1;
-    }
-    
-    
-    while (ptr1 < subArrayOne) {
-        vec[ptr3][col] = arr1[ptr1];
-        ptr1 += 1;
-        ptr3 += 1;
-    }
-    
-    
-    while (ptr2 < subArrayTwo) {
-        vec[ptr3][col] = arr2[ptr2];
-        ptr2 += 1;
-        ptr3 += 1;
+void swap_row(vector<int>&v1, vector<int> &v2){
+    for(int i=0;i<v1.size();i++){
+        v1[i] ^= v2[i] ^= v1[i] ^= v2[i];
     }
 }
-
-void MergeSort(vector<vector<int>> &v,int start,int end, int col){
-    if(start>=end) return;
-    int mid=start+(end-start)/2;
-    MergeSort(v,start,mid,col);
-    MergeSort(v,mid+1,end,col);
-    Merge(v,start,mid, end,col);
-}
-
-void display(vector<vector<int>> &matrix) {
-    for(int i = 0; i < matrix.size(); i++) {
-        for(int j = 0; j < matrix[0].size(); j++) {
-            cout << matrix[i][j] << " ";
+void cycle_sort(vector<vector<int>>&v,vector<pair<int,int>>&p, vector<vector<int>> temp){
+    for(int i = 0; i < p.size(); i++) {
+        if(p[i].second == i) continue;
+        for(int j = 0; j < v[i].size(); j++) {
+            v[i][j] = temp[p[i].second][j];
         }
-        cout << endl;
     }
 }
-
+void _sort(vector<vector<int>>&v,int col){
+    vector<pair<int,int>> arr(v.size());
+    for(int i=0;i<v.size();i++){
+        arr[i].first=v[i][col];
+        arr[i].second=i;
+    }
+    sort(arr.begin(),arr.end());
+    cycle_sort(v,arr, v);
+}
 int main()
 {   
     vector<vector<int>> matrix;
+    int n,m;
     srand(time(0));
-    int n, m;
+    cout << "enter dimensions -\n";
     cin >> n >> m;
-    matrix = create_matrix(n,m);
-    cout << "Before" << endl;
+    matrix=create_matrix(n,m);
+    cout << "before -\n";
     display(matrix);
-    int col; 
-    cout << "Enter col to sort: ";
-    cin >> col; 
-    MergeSort(matrix, 0, n - 1, col);
-    cout << "After sorting col: " << col << endl;
+    cout << " enter col to sort -\n";
+    int col;
+    cin >> col;
+    _sort(matrix,col);
+    cout << "after sorting \n";
     display(matrix);
-    return 0;
 }
